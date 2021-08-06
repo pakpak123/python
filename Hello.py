b75b6e11078264f01e24de18411d398ea54a10dc
dnskskxndnj <69186140+pakpak123@users.noreply.github.com>
2021-08-06 13:24:30 +0700

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,20 @@
+dog = 'animal'
+print(dog)
+a = ('Hello','Thailand')
+if 'Hello' in a:
+    print('OH MY GOD')
+else:
+    print('EASY')
+
+#i=7ถึงi=15
+#continueข้ามการทำงานรอบนั้นไป
+#breakออกจากการทำงานตานั้นๆ
+for i in range(7,15):
+    if i % 3 ==0:
+        continue
+    elif i == 14 :
+        break
+    print(i)
+
+
+
